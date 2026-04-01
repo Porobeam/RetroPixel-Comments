dd88f0420696aa423a3664ff569f19e670dc8a83
Porobeam <fzgbeam@gmail.com>
2026-03-31 22:09:31 -0600

    Read left buttton hidden to back
    Al preisonar la parte izquierda del read te devuelve a la principal

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,17 +1,23 @@
-document.addEventListener("DOMContentLoaded", function()
-{
+document.addEventListener("DOMContentLoaded", function() {
     let comments = document.querySelectorAll('.comment');
 
-    comments.forEach(function(comment)
-    {
-        let backgroundColor = comment.style.backgroundColor;
+    comments.forEach(function(comment) {
+        let backgroundColor = window.getComputedStyle(comment).backgroundColor;
         comment.style.backgroundColor = applyOpacity(backgroundColor);
     });
 
-    function applyOpacity(color)
-    {
+    function applyOpacity(color) {
         let opacity = 0.75;
-        let rgbaColor = color.replace(')', ', ' + opacity + ')').replace('rgb', 'rgba');
-        return rgbaColor;
+        if (color.startsWith('rgb(')) {
+            return color.replace('rgb', 'rgba').replace(')', `, ${opacity})`);
+        }
+        return color;
     }
+
+    document.addEventListener("click", function(e) {
+        let leftZone = window.innerWidth * 0.50;
+        if (e.clientX <= leftZone) {
+            window.location.href = "/";
+        }
+    });
 });
